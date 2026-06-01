20166ff5ca6fde5fdcec0bb8692a56be15ffd53e
Allan Matias <alangiovanepaes@gmail.com>
2026-06-01 17:03:24 -0300

    chore: file watcher

--- Core/src/main/java/dev/whisker/core/monitoring/domain/FileWatcher.java
+++ Core/src/main/java/dev/whisker/core/monitoring/domain/FileWatcher.java
@@ -1,30 +1,66 @@
 package dev.whisker.core.monitoring.domain;
 
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+import static java.nio.file.StandardWatchEventKinds.*;
+
+@Slf4j
+@Data
 public class FileWatcher {
     private boolean running;
+    private WatchService watcher = FileSystems.getDefault().newWatchService();
+    private final Path workdir;
+
 
     public void start() {
         running = true;
 
         Thread.ofVirtual().start(() -> {
-            System.out.println("[WATCHER] iniciado");
-
             while (running) {
                 try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    break;
+                    log.info("[WATCHER] started");
+                    watch();
+                } catch (InterruptedException | IOException e) {
+                    log.error("[WATCHER] failed");
+                    log.error("An error occurred while watching files.");
+                    log.error(e.getMessage());
+                    this.stop();
+                    throw new RuntimeException(e);
                 }
             }
-
-            System.out.println("[WATCHER] finalizado");
         });
+
+        this.stop();
     }
 
 
     public void stop() {
+        Thread.currentThread().interrupt();
         running = false;
+    }
+
+    private void watch() throws IOException, InterruptedException {
+            this.workdir.register(watcher, ENTRY_CREATE);
+            this.workdir.register(watcher, ENTRY_MODIFY);
+            this.workdir.register(watcher, ENTRY_DELETE);
+
+            WatchKey key;
+            while ((key = watcher.take()) != null) {
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    log.info("[WATCHER] Event kind: {}", event.kind());
+                    log.info("[WATCHER] Event file: {}", event.context());
+                }
+                key.reset();
+            }
+
+    }
+
+    public FileWatcher(Path workdir) throws IOException {
+        this.workdir = workdir;
     }
 }
